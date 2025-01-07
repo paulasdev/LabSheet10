ad2900d337d0cf5156b567fad4f115faaa2d984a
Paula Carolina Silva <paulacarolinasilva@Paulas-MacBook-Air.local>
2025-01-07 16:55:41 +0000

    Declare an array to hold names of beatles.

---
+++ Ex2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Declare an array to hold names of beatles.
+//Use a for loop to iterate through this array and output.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] beatles = new string[4] { "John", "Paul", "George", "Ringo"};
+
+      for (int i = 0; i < beatles.Length; i++)
+      {
+        Console.WriteLine(beatles[i]);
+      }
+      Console.ReadLine();
+    }
+
+  }
+}
+
