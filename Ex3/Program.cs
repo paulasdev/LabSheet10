6b402209cd235fb85772313d1e08c3f764ed6aef
Paula Carolina Silva <paulacarolinasilva@paulas-air.home>
2025-01-15 00:07:17 +0000

    Declare an array to hold the marks in an exam, loop through an total

---
+++ Ex3/Program.cs
@@ -0,0 +1,29 @@
+﻿//Declare an array to hold the marks in an exam, loop through an total
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+     int[] grades = new int [5] {45, 55, 60, 81, 39};
+
+     double total = 0;
+
+     for (int i = 0; i < grades.Length; i++)
+     {
+        total += grades[i]; //total = total + grades[i]
+     }  
+     Console.WriteLine("The total is {0}", total);
+     Console.WriteLine("The avarage is {0}", total/grades.Length);
+     Console.ReadLine();
+    }
+
+  }
+}
