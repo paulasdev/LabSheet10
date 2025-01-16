5c9fb71dbe537693d864e525d3c87457791d9d8e
Paula Carolina Silva <paulacarolinasilva@paulas-air.home>
2025-01-16 21:39:07 +0000

    Amend the program above so that it can read in the data from the user when prompted for each days rainfall.

---
+++ Ex7/Program.cs
@@ -0,0 +1,42 @@
+﻿//Amend the program above so that it can read in the data from the user when prompted for each days rainfall. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex7
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      
+      string[] days = new string[7] {"Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+      double[] rainfall = new double[7];
+
+      double total = 0;
+      
+
+      for (int i = 0; i < rainfall.Length; i++)
+      {
+        Console.WriteLine("Enter rainfall for {0} >>", days[i]);
+        rainfall[i] = Convert.ToDouble(Console.ReadLine());
+        
+      }
+      Console.ReadLine();
+
+        for (int i = 0; i < rainfall.Length; i++)
+      {
+        Console.WriteLine("The rainfall for {0} is {1}", days[i],rainfall[i]);
+        total = total + rainfall[i];
+      } 
+
+      Console.WriteLine("\nTotal rainfall is {0}", total);
+      Console.WriteLine("Average daily rainfall is {0:F1}", total / rainfall.Length);
+
+      Console.ReadLine();
+    }
+  }
+}
